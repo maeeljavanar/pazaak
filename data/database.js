570b66103f7e64c141ab080e8d6f65240785334b
LiamBewley <m.j.maeeljavanar@gmail.com>
2021-11-03 17:25:27 -0400

    Add database.js
    Add database.js to handle database connection and query execution

---
+++ data/database.js
@@ -0,0 +1,52 @@
+const confg = require('../config.json');
+var mysql = require('mysql2');
+var connection;
+
+/**
+ * Open/close connection for each call for now
+ * Later can rewrite to use connection pool
+ */
+function connect() {
+    connection = mysql.createConnection({
+        host: confg.dbHost,
+        user: confg.dbUser,
+        password: confg.dbPassword,
+        database: confg.dbName
+    });
+    
+    connection.connect(function(err) {
+        if (err) {
+            return console.error('error: ' + err.message);
+        } else {
+            console.log('Connected to the MySQL server.');
+        }
+    });
+}
+
+function close() {
+    connection.end(function(err) {
+        if (err) {
+            console.log('error:' + err.message);
+        } else {
+            console.log('Close the database connection.');
+        }
+    });
+}
+
+//catch all for now
+exports.executeQuery = function(sql, vals) {
+    connect();
+
+    connection.execute(sql, vals, (err, rows) => {
+       if(err) {
+           console.log("Error: " + JSON.stringify(err));
+       }
+
+        connection.unprepare(sql); //close cached statement
+        close(); //close connection
+        if(rows) {
+            console.log("Response: " + JSON.stringify(rows));
+            return rows;
+        }
+    });   
+}
