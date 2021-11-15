007cbc5abc7cfd08592b35895ee7de4ac46fbe0a
LiamBewley <m.j.maeeljavanar@gmail.com>
2021-11-15 16:33:54 -0500

    Create DAO for the hand

---
+++ data/handDB.js
@@ -0,0 +1,34 @@
+var database = require('./database.js');
+
+exports.createCard = async function(gameid, playerid, card) {
+    //insert into database
+    let response = await database.executeQuery("INSERT INTO hand_card VALUES(?, ?, ?)", [gameid, playerid, card]);
+    console.log("Response from insert: ", response);
+    if(response.affectedRows == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+exports.removeCard = async function(gameid, playerid, card) {
+    //delete
+    let response = await database.executeQuery("DELETE FROM hand_card WHERE gameid = ? AND playerid = ? AND card_code LIKE ?", [gameid, playerid, card]);
+    console.log("Response from delete: ", response);
+    if(response.affectedRows == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+exports.getCards = async function(gameid, playerid) {
+    //select
+    let cards = [];
+    let response = await database.executeQuery("SELECT card_code FROM hand_card WHERE gameid = ? AND playerid = ?", [gameid, playerid]);
+    response.forEach(row => {
+        console.log("Row: ", row);
+        cards.push(row.card_code);
+    });
+    return cards;
+}
